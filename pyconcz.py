8e8e882b3e7f4da246dc95269d33f4e064b192fa
Honza Král <honza.kral@gmail.com>
2018-06-01 15:07:51 +0200

    nother conway

--- pyconcz.py
+++ pyconcz.py
@@ -1,2 +1,51 @@
-def add(x, y):
-    return x + y
+def cell_alive(current, live_neighbors):
+
+    # Live with < 2 : die
+    if current and live_neighbors < 2:
+        return False
+
+    # Live with 2 or 3 : live
+    if current and live_neighbors in (2, 3):
+        return True
+
+    # Dead with == 3 : live
+    if not current and live_neighbors == 3:
+        return True
+
+    # Live with > 3 : die
+    if current and live_neighbors > 3:
+        return False
+
+
+def get_neighbors(coords):
+    x, y = coords
+    return {
+        (x-1, y-1), (x-1, y), (x-1, y+1),
+        (x, y-1),         (x, y+1),
+        (x+1, y-1), (x+1, y), (x+1, y+1),
+    }
+
+def alive_neighbors(coords, grid):
+    neighbors = get_neighbors(coords)
+    return sum(n in grid for n in neighbors)
+
+def grid_to_str(grid, size):
+    lines = []
+    for y in range(size):
+        line = []
+        for x in range(size):
+            if (x, y) in grid:
+                line.append('#')
+            else:
+                line.append(' ')
+        lines.append(''.join(line))
+    return '\n'.join(lines)
+
+def step(grid, size):
+    return {
+        (x, y)
+        for x in range(size)
+        for y in range(size)
+        if cell_alive((x, y) in grid, alive_neighbors((x, y), grid))
+    }
+
--- test_pyconcz.py
+++ test_pyconcz.py
@@ -1,4 +1,42 @@
 import pyconcz
 
-def test_add():
-    assert 4 == pyconcz.add(2, 2)
+
+def test_get_all_neighbors():
+    start = (1, 1)
+    expected = {
+        (0, 0), (0, 1), (0, 2),
+        (1, 0),         (1, 2),
+        (2, 0), (2, 1), (2, 2),
+    }
+
+    assert expected == set(pyconcz.get_neighbors(start))
+
+
+def test_alive_neighbors():
+    grid = {(0, 0), (1, 0)}
+
+    assert 2 == pyconcz.alive_neighbors((1, 1), grid)
+
+def test_cell_alive():
+    assert not pyconcz.cell_alive(True, 1)
+    assert pyconcz.cell_alive(True, 2)
+    assert pyconcz.cell_alive(True, 3)
+    assert not pyconcz.cell_alive(True, 4)
+
+    assert not pyconcz.cell_alive(False, 2)
+    assert pyconcz.cell_alive(False, 3)
+    assert not pyconcz.cell_alive(False, 4)
+
+def test_grid_to_str():
+    assert " # \n#  \n   " == pyconcz.grid_to_str({
+        (0, 1), (1, 0)
+        }, 3)
+
+def test_step():
+    grid = set()
+    for x in range(10):
+        for y in range(10):
+            grid.add((x, y))
+
+    assert {(0, 0), (9, 0), (0, 9), (9, 9)} == pyconcz.step(grid, 10)
+
